2ab2666f010cf09b5c9d71a899a61a53c1e9a340
Brunno Goncalves <brunnocgoncalves@gmail.com>
2016-11-14 18:11:42 -0200

    DropShadow protocol added

---
+++ TacoPop/DropShadow.swift
@@ -0,0 +1,17 @@
+//
+//  DropShadow.swift
+//  TacoPop
+//
+//  Created by Brunno Goncalves on 14/11/16.
+//  Copyright © 2016 brunnogoncalves. All rights reserved.
+//
+
+import UIKit
+
+protocol DropShadow {}
+
+extension DropShadow where Self:UIView{
+    
+    
+    
+}
